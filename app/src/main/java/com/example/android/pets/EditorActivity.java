e567569084adaf5973345795e14407aa44e4f12f
Omar <sx40hs@gmail.com>
2020-02-25 15:25:25 +0300

    Pets app starting Point

---
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -0,0 +1,43 @@
+package com.example.android.pets;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class EditorActivity extends AppCompatActivity {
+
+    /** EditText field to enter the pet's name */
+    private EditText mNameEditText;
+
+    /** EditText field to enter the pet's breed */
+    private EditText mBreedEditText;
+
+    /** EditText field to enter the pet's weight */
+    private EditText mWeightEditText;
+
+    /** EditText field to enter the pet's gender */
+    private Spinner mGenderSpinner;
+
+    /**
+     * Gender of the pet. The possible values are:
+     * 0 for unknown gender, 1 for male, 2 for female.
+     */
+    private int mGender = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+    }
+
+}
